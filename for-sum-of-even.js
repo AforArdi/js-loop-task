0de87a72667e8035ee47826b29116f497ea98de0
AforArdi <binsbro36@gmail.com>
2026-01-28 07:32:47 +0600

    sum of odd and even using for loop

---
+++ for-sum-of-even.js
@@ -0,0 +1,8 @@
+let sum = 0;
+
+for (let i = 51; i <= 85; i++){
+    if(i%2===0){
+        sum+=i;
+    }
+}
+console.log(sum);
---
+++ for-sum-of-odd.js
@@ -0,0 +1,8 @@
+let sum = 0;
+
+for (let i = 91; i <= 129; i++){
+    if(i%2===1){
+        sum+=i;
+    }
+}
+console.log(sum);
